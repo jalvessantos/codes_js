ae82d6fff1796eff8a4e98c1cc9de84f6e196ab8
José <jdbalves@gmail.com>
2022-07-25 19:40:29 -0300

    Exemplos JS

---
+++ typeBoolean.js
@@ -0,0 +1,13 @@
+
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivo = false;
+
+///console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Alura";
+const texto2 = "Alura";
+
+console.log(texto1 === texto2);
+
